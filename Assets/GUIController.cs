877aed6ed854cd66219359e1c7a170f9c9bf7e35
ewdlop <ray810815@gmail.com>
2020-05-02 22:45:54 -0400

    Learning Rate and Discounting Factor Slider

--- Assets/GUIController.cs
+++ Assets/GUIController.cs
@@ -12,27 +12,35 @@ public class GUIController : MonoBehaviour
     private Text _iterationText;
     [SerializeField]
     private Text _stepText;
+    [SerializeField]
     private Text _LearningRateText;
+    [SerializeField]
     private Text _DiscountingFactorText;
     public Text IterationText { get => _iterationText; set => _stepText = value; }
     public Text StepText { get => _stepText; set => _stepText = value; }
+    public Text LearningRateText { get => _LearningRateText; set => _LearningRateText = value; }
+    public Text DiscountingFactorText { get => _DiscountingFactorText; set => _DiscountingFactorText = value; }
 
     public void UpdateInterationText(string newText)
     {
-        IterationText.text = string.Format("Iterion: {0}", newText);
+        IterationText.text = string.Format("Iteration: {0}", newText);
     }
 
     public void UpdateStepText(string newText)
     {
         StepText.text = string.Format("Step: {0}", newText);
     }
+    public void UpdateLearningRateValue(float newValue)
+    {
+        LearningRateText.text = string.Format("LearningRate: {0}", newValue);
+    }
 
+    public void UpdateDiscountingFactorValue(float newValue)
+    {
+        DiscountingFactorText.text = string.Format("Discounting Factor: {0}", newValue);
+    }
     public void ResetScene()
     {       
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-    public void UpdateLearningRateValue(float newValue)
-    {
-        StepText.text = string.Format("Step: {0}", newValue);
     }
 }
